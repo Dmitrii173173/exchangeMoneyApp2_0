bd77a30c1ace0e255cb824b424f0b3258600fdab
Dmitrii <dmitrii.perfilev2020@gmail.com>
2023-06-20 15:48:36 +0800

    End of section 2

---
+++ -- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Dmitrii");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Aristan");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "BaurzhanK");
