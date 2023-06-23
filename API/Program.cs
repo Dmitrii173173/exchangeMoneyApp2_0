586fe22ec5361a029323257b46ebb0748dda4def
Dmitrii <dmitrii.perfilev2020@gmail.com>
2023-06-24 05:03:45 +0800

    Step 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,7 +12,11 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("Https://localhost:4200"));
 
 app.MapControllers();
 
